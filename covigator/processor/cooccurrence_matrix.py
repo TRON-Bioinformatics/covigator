ac466b0c14f89483189a2f9c513438d5003291f8
priesgof <priesgoferreiro@gmail.com>
2021-04-13 00:11:06 +0200

    deal with race conditions in the calculation of the co-occurrence matrix

--- covigator/processor/cooccurrence_matrix.py
+++ covigator/processor/cooccurrence_matrix.py
@@ -3,6 +3,11 @@ from sqlalchemy.orm import Session
 from covigator.database.model import Sample, VariantCooccurrence
 from logzero import logger
 from covigator.database.queries import Queries
+from sqlalchemy.exc import IntegrityError
+
+
+class CooccurrenceMatrixException(Exception):
+    pass
 
 
 class CooccurrenceMatrix:
@@ -19,19 +24,35 @@ class CooccurrenceMatrix:
 
         # the order by position is important to ensure we store only half the matrix and the same half of the matrix
         variants = queries.get_variants_by_sample(sample_id)
+        failed_variants = []
         for (variant_one, variant_two) in combinations(variants, 2):
+            try:
+                variant_cooccurrence = queries.get_variant_cooccurrence(variant_one, variant_two)
+                if variant_cooccurrence is None:
+                    variant_cooccurrence = VariantCooccurrence(
+                        chromosome_one=variant_one.chromosome,
+                        position_one=variant_one.position,
+                        reference_one=variant_one.reference,
+                        alternate_one=variant_one.alternate,
+                        chromosome_two=variant_two.chromosome,
+                        position_two=variant_two.position,
+                        reference_two=variant_two.reference,
+                        alternate_two=variant_two.alternate,
+                        count=1
+                    )
+                    session.add(variant_cooccurrence)
+                else:
+                    # NOTE: it is important to increase the counter like this to avoid race conditions
+                    # the increase happens in the database server and not in python
+                    # see https://stackoverflow.com/questions/2334824/how-to-increase-a-counter-in-sqlalchemy
+                    variant_cooccurrence.count = VariantCooccurrence.count + 1
+            except IntegrityError:
+                session.rollback()
+                failed_variants.append((variant_one, variant_two))
+
+        # tries again the failed variants as these are expected to be there now
+        for (variant_one, variant_two) in failed_variants:
             variant_cooccurrence = queries.get_variant_cooccurrence(variant_one, variant_two)
             if variant_cooccurrence is None:
-                variant_cooccurrence = VariantCooccurrence(
-                    chromosome_one=variant_one.chromosome,
-                    position_one=variant_one.position,
-                    reference_one=variant_one.reference,
-                    alternate_one=variant_one.alternate,
-                    chromosome_two=variant_two.chromosome,
-                    position_two=variant_two.position,
-                    reference_two=variant_two.reference,
-                    alternate_two=variant_two.alternate,
-                    count=0
-                )
-                session.add(variant_cooccurrence)
-            variant_cooccurrence.count += 1
+                raise CooccurrenceMatrixException("Some cooccurrent variants failed to be persisted twice")
+            variant_cooccurrence.count = VariantCooccurrence.count + 1
