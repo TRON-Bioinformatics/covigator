95fc9de98aef98f9d7f7ad401a6a572158cfb24a
priesgof <priesgoferreiro@gmail.com>
2021-05-12 08:20:51 +0200

    fix tests

--- covigator/tests/unit_tests/test_queries.py
+++ covigator/tests/unit_tests/test_queries.py
@@ -139,8 +139,10 @@ class QueriesTests(TestCase):
         self.assertIsNotNone(data)
         num_unique_variants = len(set([v.variant_id for v in variants]))
         self.assertEqual(data.shape[0], num_unique_variants * num_unique_variants)
-        self.assertEqual(data.shape[1], 5)
+        self.assertEqual(data.shape[1], 6)
         self.assertGreater(data[data["count"] > 0].shape[0], len(variants))
+        self.assertGreater(data[data["frequency"] > 0].shape[0], len(variants))
+        self.assertGreater(data[data["jaccard"] > 0].shape[0], len(variants))
 
         data = self.queries.get_variants_cooccurrence_by_gene(gene_name="S", min_cooccurrence=11, test=True)
         self.assertIsNone(data)
@@ -150,8 +152,10 @@ class QueriesTests(TestCase):
 
         data = self.queries.get_variants_cooccurrence_by_gene(gene_name="N", min_cooccurrence=1, test=True)
         self.assertIsNotNone(data)
-        self.assertEqual(data.shape[1], 5)
+        self.assertEqual(data.shape[1], 6)
         self.assertGreater(data[data["count"] > 0].shape[0], len(other_variants))
+        self.assertGreater(data[data["frequency"] > 0].shape[0], len(variants))
+        self.assertGreater(data[data["jaccard"] > 0].shape[0], len(variants))
 
     def test_get_variant_abundance_histogram(self):
 
