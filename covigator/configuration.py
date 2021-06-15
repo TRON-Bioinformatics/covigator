d3c0a00c75660b6aac836b26d174b672add712ca
priesgof <priesgoferreiro@gmail.com>
2021-06-15 14:58:15 +0200

    integrate nextflow workflow

--- covigator/configuration.py
+++ covigator/configuration.py
@@ -6,8 +6,10 @@ from covigator.exceptions import CovigatorDashBoardInitialisationError
 
 class Configuration:
 
-    # configuration environment variables
+    # file system storage
     ENV_COVIGATOR_STORAGE_FOLDER = "COVIGATOR_STORAGE_FOLDER"
+    ENV_COVIGATOR_TEMP_FOLDER = "COVIGATOR_TEMP_FOLDER"
+    # database
     ENV_COVIGATOR_DB_PORT = "COVIGATOR_DB_PORT"
     ENV_COVIGATOR_DB_PASSWORD = "COVIGATOR_DB_PASSWORD"
     ENV_COVIGATOR_DB_USER = "COVIGATOR_DB_USER"
@@ -15,27 +17,22 @@ class Configuration:
     ENV_COVIGATOR_DB_HOST = "COVIGATOR_DB_HOST"
     ENV_COVIGATOR_DB_POOL_SIZE = "COVIGATOR_DB_POOL_SIZE"
     ENV_COVIGATOR_DB_MAX_OVERFLOW = "COVIGATOR_DB_MAX_OVERFLOW"
+    ENV_COVIGATOR_TABLE_VERSION = "COVIGATOR_TABLE_VERSION"
+    # dashboard
     ENV_COVIGATOR_DASHBOARD_HOST = "COVIGATOR_DASHBOARD_HOST"
     ENV_COVIGATOR_DASHBOARD_PORT = "COVIGATOR_DASHBOARD_PORT"
+    # logs
     ENV_COVIGATOR_DASHBOARD_LOG_FILE = "COVIGATOR_DASHBOARD_LOG_FILE"
     ENV_COVIGATOR_PROCESSOR_LOG_FILE = "COVIGATOR_PROCESSOR_LOG_FILE"
     ENV_COVIGATOR_ACCESSOR_LOG_FILE = "COVIGATOR_ACCESSOR_LOG_FILE"
-    ENV_COVIGATOR_BIN_FASTP = "COVIGATOR_BIN_FASTP"
-    ENV_COVIGATOR_BIN_SAMTOOLS = "COVIGATOR_BIN_SAMTOOLS"
-    ENV_COVIGATOR_BIN_BWA = "COVIGATOR_BIN_BWA"
-    ENV_COVIGATOR_BIN_BCFTOOLS = "COVIGATOR_BIN_BCFTOOLS"
-    ENV_COVIGATOR_BIN_SNPEFF = "COVIGATOR_BIN_SNPEFF"
-    ENV_COVIGATOR_BIN_BGZIP = "COVIGATOR_BIN_BGZIP"
-    ENV_COVIGATOR_BIN_TABIX = "COVIGATOR_BIN_TABIX"
-    ENV_COVIGATOR_BIN_JAVA = "COVIGATOR_BIN_JAVA"
+    # pipeline
+    ENV_COVIGATOR_WORKFLOW = "COVIGATOR_WORKFLOW"
+    ENV_COVIGATOR_WORKFLOW_VERSION = "COVIGATOR_WORKFLOW_VERSION"
+    # references
     ENV_COVIGATOR_REF_FASTA = "COVIGATOR_REF_FASTA"
-    ENV_COVIGATOR_REF_GISAID = "COVIGATOR_REF_GISAID"
-    ENV_COVIGATOR_SEQ_GISAID = "COVIGATOR_SEQ_GISAID"
-    ENV_COVIGATOR_META_GISAID = "COVIGATOR_META_GISAID"
-    ENV_COVIGATOR_DASK_PORT = "COVIGATOR_DASK_PORT"
-
-    ENV_COVIGATOR_TABLE_VERSION = "COVIGATOR_TABLE_VERSION"
     ENV_COVIGATOR_GENE_ANNOTATIONS = "COVIGATOR_GENE_ANNOTATIONS"
+    # dask
+    ENV_COVIGATOR_DASK_PORT = "COVIGATOR_DASK_PORT"
 
     def __init__(self):
         # local storage
@@ -66,19 +63,13 @@ class Configuration:
         self.logfile_accesor = os.getenv(self.ENV_COVIGATOR_ACCESSOR_LOG_FILE)
         self.logfile_processor = os.getenv(self.ENV_COVIGATOR_PROCESSOR_LOG_FILE)
 
-        # pipeline binaries
-        self.fastp = os.getenv(self.ENV_COVIGATOR_BIN_FASTP, "/code/fastp/fastp")
-        self.samtools = os.getenv(self.ENV_COVIGATOR_BIN_SAMTOOLS, "/code/samtools/1.9/samtools")
-        self.bwa = os.getenv(self.ENV_COVIGATOR_BIN_BWA, "/code/bwa/0.7.17/bwa")
-        self.bcftools = os.getenv(self.ENV_COVIGATOR_BIN_BCFTOOLS, "/code/bcftools/1.9/bcftools")
-        self.snpeff = os.getenv(self.ENV_COVIGATOR_BIN_SNPEFF, "/code/snpEFF_latest_core/snpEff/snpEff.jar")
-        self.bgzip = os.getenv(self.ENV_COVIGATOR_BIN_BGZIP, "bgzip")
-        self.tabix = os.getenv(self.ENV_COVIGATOR_BIN_TABIX, "tabix")
-        self.java = os.getenv(self.ENV_COVIGATOR_BIN_JAVA, "java")
-
         # references
         self.reference_genome = os.getenv(self.ENV_COVIGATOR_REF_FASTA)
         self.reference_gene_annotations = os.getenv(self.ENV_COVIGATOR_GENE_ANNOTATIONS)
+
+        self.workflow = os.getenv(self.ENV_COVIGATOR_WORKFLOW, "tron-bioinformatics/covigator-ngs-pipeline")
+        self.workflow_version = os.getenv(self.ENV_COVIGATOR_WORKFLOW_VERSION, "v0.3.0")
+        self.temp_folder = os.getenv(self.ENV_COVIGATOR_TEMP_FOLDER, "/data/covigator-tmp")
 
 
 def initialise_logs(logfile, sample_id: str = None):
--- covigator/pipeline/ena_pipeline.py
+++ covigator/pipeline/ena_pipeline.py
@@ -2,7 +2,6 @@ import os
 import time
 from pathlib import Path
 import subprocess
-import tempfile
 from logzero import logger
 from covigator.configuration import Configuration
 from covigator.exceptions import CovigatorPipelineError
@@ -13,40 +12,27 @@ class Pipeline:
     def __init__(self, config: Configuration):
         self.config = config
 
-    def run(self, fastq1: str, fastq2: str = None):
+    def run(self, run_accession: str, fastq1: str, fastq2: str = None):
 
         logger.info("Processing {} and {}".format(fastq1, fastq2))
         sample_data_folder = Path(fastq1).parent
-        bwa_ref = self.config.reference_genome
 
         logger.info("Sample data folder: {}".format(sample_data_folder))
-        bam_file = os.path.join(sample_data_folder, "aligned.out.bam")
-        vcf_file = os.path.join(sample_data_folder, "pileup.vcf")
-        snpeff_vcf_file_gz = os.path.join(sample_data_folder, "snpeff.vcf.gz")
+        snpeff_vcf_file_gz = os.path.join(
+            sample_data_folder, "{name}.lofreq.normalized.annotated.vcf.gz".format(name=run_accession))
 
-        if fastq2:
-            cmd_align = "{} mem {} {} {} | {} sort -o {} -".format(
-                self.config.bwa, bwa_ref, fastq1, fastq2, self.config.samtools, bam_file)
-        else:
-            cmd_align = "{} mem {} {} | {} sort -o {} -".format(
-                self.config.bwa, bwa_ref, fastq1, self.config.samtools, bam_file)
+        command = "nextflow run {workflow} -r {workflow_version} " \
+                  "--fastq1 {fastq1} {fastq2} --output {output_folder} --name {name} " \
+                  "-profile conda -offline -work-dir {work_folder}".format(
+            fastq1=fastq1, fastq2="--fastq2 " + fastq2 if fastq2 else "", output_folder=sample_data_folder,
+            name=run_accession, work_folder=self.config.temp_folder,
+            workflow_version=self.config.workflow_version, workflow=self.config.workflow)
 
-        cmd_pileup = "{0} mpileup -E -d 0 -A -f {1} {2} | {0} call -mv --ploidy 1 -Ov -o {3}".format(
-            self.config.bcftools, bwa_ref, bam_file, vcf_file)
-
-        cmd_snpeff = "{java} -jar {snpeff} ann " \
-                     "-noStats -no-downstream -no-upstream -no-intergenic -no-intron -onlyProtein " \
-                     "SARS-COV2 {input_vcf} | {bgzip} -c > {annotated_vcf} && {tabix} -p vcf {annotated_vcf}"\
-            .format(java=self.config.java, snpeff=self.config.snpeff, input_vcf=vcf_file, bgzip=self.config.bgzip,
-                    annotated_vcf=snpeff_vcf_file_gz, tabix=self.config.tabix)
-
-        self._run_commands([cmd_align, cmd_pileup, cmd_snpeff], sample_data_folder)
-        self.delete_files([bam_file, vcf_file])
+        self._run_command(command, sample_data_folder)
 
         return snpeff_vcf_file_gz
 
-    def _run_commands(self, commands, temporary_folder):
-        for command in commands:
+    def _run_command(self, command, temporary_folder):
             start = time.time()
             p = subprocess.Popen(
                 command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=temporary_folder, shell=True)
@@ -59,7 +45,3 @@ class Pipeline:
 
     def _decode(self, data):
         return data.decode("utf8")
-
-    def delete_files(self, files):
-        for f in files:
-            os.remove(f)
