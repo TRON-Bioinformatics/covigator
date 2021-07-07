c5346fee03e41edf43c80c63eaef773d3800f25b
priesgof <priesgoferreiro@gmail.com>
2021-07-07 21:57:08 +0200

    implement GISAID exclusion rules based on horizontal coverage and quality of sequence
    Former-commit-id: c34537fdacfea10e16ae39ec40e4ec763096223e

--- scripts/exclude_samples.sql
+++ scripts/exclude_samples.sql
@@ -17,3 +17,11 @@ update job_gisaid_v12 set status='EXCLUDED' where run_accession in (
 update job_gisaid_v12 set status='EXCLUDED' where run_accession in (
     select run_accession from (select count(*) as count, sample from variant_observation_v12
     where length(reference) = 1 and length(alternate) > 1 group by sample) as counts where count > 10);
+
+-- exclude samples with a ratio of Ns or ambiguous bases greater than 0.2 or a sequence length < 20 % of the genome
+update job_gisaid_v14 set status='EXCLUDED' where run_accession in (
+    select run_accession from (
+        select cast(sequence_length as float) / 29903 as coverage_ratio,
+            cast(count_n_bases + count_ambiguous_bases as float) / sequence_length as n_ratio
+        from sample_gisaid_v14)
+    as counts where n_ratio > 0.2 or coverage_ratio < 0.2);
