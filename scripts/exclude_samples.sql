57db0f490d3e6cc5ac570c351a65d7f237b25075
priesgof <priesgoferreiro@gmail.com>
2021-07-07 22:01:39 +0200

    amend exclusion SQL query
    Former-commit-id: caf4c148f434cc5c7c8431cf50d10c162dc763b3

--- scripts/exclude_samples.sql
+++ scripts/exclude_samples.sql
@@ -21,7 +21,8 @@ update job_gisaid_v12 set status='EXCLUDED' where run_accession in (
 -- exclude samples with a ratio of Ns or ambiguous bases greater than 0.2 or a sequence length < 20 % of the genome
 update job_gisaid_v14 set status='EXCLUDED' where run_accession in (
     select run_accession from (
-        select cast(sequence_length as float) / 29903 as coverage_ratio,
+        select run_accession,
+            cast(sequence_length as float) / 29903 as coverage_ratio,
             cast(count_n_bases + count_ambiguous_bases as float) / sequence_length as n_ratio
         from sample_gisaid_v14)
     as counts where n_ratio > 0.2 or coverage_ratio < 0.2);
