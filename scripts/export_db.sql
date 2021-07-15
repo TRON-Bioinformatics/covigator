a28f920bed447e99f0da932fd847b31e9df45eae
Pablo Riesgo Ferreiro <pablo.riesgoferreiro@tron-mainz.de>
2021-07-15 10:19:15 +0200

    update DB import/export scripts
    Former-commit-id: 8267a8ce66660eae452b5cd2d386df894ee6c2db

--- scripts/export_db.sql
+++ scripts/export_db.sql
@@ -10,9 +10,25 @@
 -- this is exporting the GISAID related tables
 \copy sample_v12 to program 'gzip > sample_v12.csv.gz' csv header;
 -- in this table we need to specifiy the columns to avoid exporting the table with the sequences which is large
-\copy sample_gisaid_v12(run_accession, date, host_tax_id, host, country_raw, region, country, country_alpha_2, country_alpha_3, continent, continent_alpha_2, site, site2) to program 'gzip > sample_gisaid_v12.csv.gz' csv header;
+\copy sample_gisaid_v12(run_accession, date, host_tax_id, host, country_raw, region, country, country_alpha_2, country_alpha_3, continent, continent_alpha_2, site, site2, sequence_length, count_n_bases, count_ambiguous_bases, count_snvs, count_insertions, count_deletions) to program 'gzip > sample_gisaid_v12.csv.gz' csv header;
 \copy job_gisaid_v12 to program 'gzip > job_gisaid_v12.csv.gz' csv header;
-\copy variant_observation_v12 to program 'gzip > variant_observation_v12.csv.gz' csv header;
+\copy variant_observation_v11 to program 'gzip > variant_observation_v11.csv.gz' csv header;
 \copy variant_v12 to program 'gzip > variant_v12.csv.gz' csv header;
+
+-- export only GISAID when there is a mix of data
+\copy (select * from variant_observation_v14 where source='GISAID') to program 'gzip > variant_observation_v14_gisaid.csv.gz' csv header;
+\copy variant_v14 to program 'gzip > variant_v14.csv.gz' csv header;
+
+
+\copy precomputed_annotation_v14 to program 'gzip > precomputed_annotation_v14.csv.gz' csv header;
+\copy precomputed_indel_length_v14 to program 'gzip > precomputed_indel_length_v14.csv.gz' csv header;
+\copy precomputed_substitutions_counts_v14 to program 'gzip > precomputed_substitutions_counts_v14.csv.gz' csv header;
+\copy precomputed_variants_per_sample_v14 to program 'gzip > precomputed_variants_per_sample_v14.csv.gz' csv header;
+\copy precomputed_top_occurrence_v14 to program 'gzip > precomputed_top_occurrence_v14.csv.gz' csv header;
+
+
+
+
+
 
 
--- scripts/import_db.sql
+++ scripts/import_db.sql
@@ -6,8 +6,14 @@ CREATE TABLE tmp_variant_v13 (LIKE variant_v13 INCLUDING DEFAULTS);
 INSERT INTO variant_v13 SELECT * FROM tmp_variant_v13 ON CONFLICT DO NOTHING;
 drop table tmp_variant_v13;
 
+
+CREATE TABLE tmp_sample_v2 (LIKE sample_v2 INCLUDING DEFAULTS);
+\copy tmp_sample_v2 from program 'gzip -dc sample_v14.csv.gz' csv header;
+INSERT INTO sample_v2 SELECT * FROM tmp_sample_v2 ON CONFLICT DO NOTHING;
+drop table tmp_sample_v2;
+
 \copy sample_ena_v13 from program 'gzip -dc sample_ena_v11.csv.gz' csv header;
-\copy sample_gisaid_v13(run_accession, date, host_tax_id, host, country_raw, region, country, country_alpha_2, country_alpha_3, continent, continent_alpha_2, site, site2) from program 'gzip -dc sample_gisaid_v12.csv.gz' csv header;
+\copy sample_gisaid_v13(run_accession, date, host_tax_id, host, country_raw, region, country, country_alpha_2, country_alpha_3, continent, continent_alpha_2, site, site2, sequence_length, count_n_bases, count_ambiguous_bases, count_snvs, count_insertions, count_deletions) from program 'gzip -dc sample_gisaid_v12.csv.gz' csv header;
 
 \copy sample_v13 from program 'gzip -dc sample_v11.csv.gz' csv header;
 \copy sample_v13 from program 'gzip -dc sample_v12.csv.gz' csv header;
@@ -19,3 +25,10 @@ drop table tmp_variant_v13;
 
 \copy job_ena_v13(run_accession,status,created_at,queued_at,downloaded_at,analysed_at,cleaned_at,loaded_at,cooccurrence_at,failed_at,error_message,fastq_path,vcf_path) from program 'gzip -dc job_ena_v11.csv.gz' csv header;
 \copy job_gisaid_v12 from program 'gzip -dc job_gisaid_v12.csv.gz' csv header;
+
+-- precomputed data
+\copy precomputed_annotation_v2 from program 'gzip -dc precomputed_annotation_v14.csv.gz' csv header;
+\copy precomputed_indel_length_v2 from program 'gzip -dc precomputed_indel_length_v14.csv.gz' csv header;
+\copy precomputed_substitutions_counts_v2 from program 'gzip -dc precomputed_substitutions_counts_v14.csv.gz' csv header;
+\copy precomputed_variants_per_sample_v2 from program 'gzip -dc precomputed_variants_per_sample_v14.csv.gz' csv header;
+\copy precomputed_top_occurrence_v2 from program 'gzip -dc precomputed_top_occurrence_v14.csv.gz' csv header;
