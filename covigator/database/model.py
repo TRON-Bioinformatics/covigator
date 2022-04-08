1ffffd450f60bff641d57c8ec4e1d918865020d2
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2022-04-08 15:35:00 +0200

    add comment about casing in the model

--- covigator/database/model.py
+++ covigator/database/model.py
@@ -304,6 +304,7 @@ class SampleEna(Base):
     pangolin_scorpio_conflict = Column(Float)
     pangolin_version = Column(String)
     pangolin_pangolin_version = Column(String)
+    # TODO: remove upper case from here, it confuses SQL for some operations
     pangolin_pangoLEARN_version = Column(String)
     pangolin_pango_version = Column(String)
     pangolin_status = Column(String)
