9d799fc576bf70ecc0a7c6ac9915231b80d15904
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2022-06-12 09:32:20 +0200

    fix download unit tests

--- covigator/tests/unit_tests/test_downloader.py
+++ covigator/tests/unit_tests/test_downloader.py
@@ -30,8 +30,8 @@ class DownloaderTest(AbstractTest):
         run_accession = "TEST12346"
         ena_run = SampleEna(
             run_accession=run_accession,
-            fastq_ftp="https://tron-mainz.de/wp-content/uploads/2020/07/TRON_Logo_Science.svg",
-            fastq_md5="ca72bacad0dfcf665df49bfc53cc8b60"
+            fastq_ftp="https://assets.website-files.com/62862274344968d3d3cb1cfc/62862274344968148ecb1d35_TRON%20Logo.svg",
+            fastq_md5="de83875d1badf03508269ff77e63e676"
         )
         self.downloader.download(ena_run)
         run_storage_folder = os.path.join(self.downloader.storage_folder, "nodate", run_accession)
@@ -43,7 +43,7 @@ class DownloaderTest(AbstractTest):
         ena_run = SampleEna(
             run_accession=run_accession,
             fastq_ftp="https://tron-mainz.de/wp-content/uploads/2020/07/TRON_Logo_Science.svg",
-            fastq_md5="ca72bacad0dfcf665df49bfc53cc8b60",
+            fastq_md5="de83875d1badf03508269ff77e63e676",
             collection_date=datetime.date(year=2021, month=6, day=30)
         )
         self.downloader.download(ena_run)
